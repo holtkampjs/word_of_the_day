83a9f9913107f7d7fcf218ace73bd7bdd6f54283
Joseph Holtkamp <holtkampjs@gmail.com>
2023-03-10 23:11:11 -0600

    Retrieves word from wordnik api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let api_key = std::env::var("WORDNIK_API_KEY").unwrap();
+    let url = "https://api.wordnik.com/v4/words.json/randomWord?api_key=".to_owned() + &api_key;
+    let response = reqwest::get(url).await?;
+    let body = response.text().await?;
+    println!("{}", body);
+    Ok(())
 }
