edd335de79e372863ce5d1d336f6f4028dc302cd
Joseph Holtkamp <holtkampjs@gmail.com>
2023-03-11 08:58:17 -0600

    Parses response into json, not yet usable

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use reqwest::Result;
+#![allow(unused)]
 
-#[tokio::main]
-async fn main() -> Result<()> {
+struct WordOfTheDay {
+    word: String,
+    category: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let api_key = std::env::var("WORDNIK_API_KEY").unwrap();
-    let url = "https://api.wordnik.com/v4/words.json/randomWord?api_key=".to_owned() + &api_key;
-    let response = reqwest::get(url).await?;
-    let body = response.text().await?;
-    println!("{}", body);
+    let url = "https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=".to_owned() + &api_key;
+    let response = reqwest::blocking::get(url)?.text()?;
+    let json: serde_json::Value = serde_json::from_str(&response)?;
+    println!("{:#?}", json);
     Ok(())
 }
