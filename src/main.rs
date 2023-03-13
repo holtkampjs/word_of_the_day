93a52a448bfd71f2e9ae2aef0731d76639463aef
Joseph Holtkamp <holtkampjs@gmail.com>
2023-03-13 15:13:35 -0500

    Moved display logic to impl block and removed serde_json as a dependency

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
 use reqwest::Result;
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Deserialize)]
 struct Definition {
-    source: String,
-    text: String,
-    note: Option<String>,
     #[serde(rename(deserialize = "partOfSpeech"))]
     part_of_speech: String,
-}
-
-#[derive(Debug, Deserialize, Serialize)]
-struct ExampleUsage {
-    url: String,
-    title: String,
     text: String,
-    id: i32,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Deserialize)]
+struct ExampleUsage {
+    text: String,
+}
+
+#[derive(Deserialize)]
 struct WordOfTheDay {
     definitions: Vec<Definition>,
     examples: Vec<ExampleUsage>,
     note: String,
     word: String,
+}
+
+impl WordOfTheDay {
+    fn display(&self) {
+        println!("Word of the Day:");
+        println!("  {}\n", self.word);
+
+        println!("Definitions:");
+        for definition in self.definitions.iter() {
+            println!("  Part of speech: {}", definition.part_of_speech);
+            println!("  Definition:");
+            println!("    {}\n", definition.text);
+        }
+
+        println!("Examples:");
+        for example in self.examples.iter() {
+            println!("  Example:");
+            println!("    {}\n", example.text);
+        }
+
+        println!("Note:");
+        println!("  {}", self.note);
+    }
 }
 
 #[tokio::main]
@@ -32,6 +50,8 @@ async fn main() -> Result<()> {
     let url = "https://api.wordnik.com/v4/words.json/wordOfTheDay?api_key=".to_owned() + &api_key;
     let word_of_the_day: WordOfTheDay =
         reqwest::Client::new().get(url).send().await?.json().await?;
-    println!("{:#?}", word_of_the_day);
+
+    word_of_the_day.display();
+
     Ok(())
 }
